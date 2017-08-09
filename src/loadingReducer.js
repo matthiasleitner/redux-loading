b6ca9a260afc9e2b8af434a7c65732dc1dd3f9ea
Matthias Leitner <matthias.leitner@lints.at>
2017-08-09 20:54:12 +0200

    allow global meta

--- src/loadingReducer.js
+++ src/loadingReducer.js
@@ -38,7 +38,7 @@ const loadingReducer = (state = {
   let   updatedLoader  =  null;
   let   message = getMessage(action);
 
-  const globalLoading = action.payload ? action.payload.global : false;
+  const globalLoading = action.payload ? action.payload.global : !!action.meta.global;
 
   switch (action.type) {
     case 'LOADING':
